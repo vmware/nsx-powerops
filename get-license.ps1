f04360752c9994cad889e16081ab8e428b92e5e7
Puneet Chawla <thisispuneet@gmail.com>
2018-08-13 23:41:45 -0700

    Fixed array issue in get-license.

--- get-license.ps1
+++ get-license.ps1
@@ -1,32 +1,35 @@
-$licenseDictionary = @{}
-$nsxManagerLicenseLabelstemp = "CostUnit", "EditionKey", "LicenseKey", "Name", "Total"
+function Get-NSXLicenseInfo {
+    $licenseDictionary = @{}
+    $nsxManagerLicenseLabelstemp = "CostUnit", "EditionKey", "LicenseKey", "Name", "Total"
+    $nsxManagerLicenseLabels = @()
 
-#Getting License info fron vCenter 
-$ServiceInstance = Get-View ServiceInstance
-$LicenseManager = Get-View $ServiceInstance.Content.licenseManager
-$details = $LicenseManager.Licenses | where-object { $_.EditionKey -match 'nsx' }
+    #Getting License info fron vCenter 
+    $ServiceInstance = Get-View ServiceInstance
+    $LicenseManager = Get-View $ServiceInstance.Content.licenseManager
+    $details = $LicenseManager.Licenses | where-object { $_.EditionKey -match 'nsx' }
 
-$tcount=1
-foreach ($eachdetail in $details){
-    $nsxManagerLicenseLabelstemp | %{
-        $newlable = [string]$_+$tcount
-        $nsxManagerLicenseLabels += $newlable
-        $licenseDictionary[$newlable]=[string]$($eachdetail.$_)
+    $tcount=1
+    foreach ($eachdetail in $details){
+        $nsxManagerLicenseLabelstemp | %{
+            $newlable = [string]$_+$tcount
+            $nsxManagerLicenseLabels += $newlable
+            $licenseDictionary[$newlable]=[string]$($eachdetail.$_)
+        }
+        $tcount++
     }
-    $tcount++
-}
 
-#Getting License info from NSX Mgr via API call
-$licenseResponse = Invoke-NsxRestMethod -URI "/api/2.0/services/licensing/capacityusage" -method get
-$licenseUsageInfo = $licenseResponse.featureCapacityUsageList.featureCapacityUsageInfo
-$licenseUsageInfo | %{
-    $myfeature = $_.feature
-    $_.capacityUsageInfo | %{
-        [String]$newCapacityType = $myfeature+"-"+$($_.capacityType)
-        $nsxManagerLicenseLabels += $newCapacityType
-        $licenseDictionary[$myfeature+"-"+$($_.capacityType)] = $_.usageCount
+    #Getting License info from NSX Mgr via API call
+    $licenseResponse = Invoke-NsxRestMethod -URI "/api/2.0/services/licensing/capacityusage" -method get
+    $licenseUsageInfo = $licenseResponse.featureCapacityUsageList.featureCapacityUsageInfo
+    $licenseUsageInfo | %{
+        $myfeature = $_.feature
+        $_.capacityUsageInfo | %{
+            [String]$newCapacityType = $myfeature+"-"+$($_.capacityType)
+            $nsxManagerLicenseLabels += $newCapacityType
+            $licenseDictionary[$myfeature+"-"+$($_.capacityType)] = $_.usageCount
+        }
     }
-}
 
-$licenseArray = $licenseDictionary, $nsxManagerLicenseLabels
-return $licenseArray
+    $licenseArray = $licenseDictionary, $nsxManagerLicenseLabels
+    return $licenseArray
+}
