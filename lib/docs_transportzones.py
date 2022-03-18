a492b3184795678bf1851b3cfd2f6d29098ee570
Dominic Foley <65222784+domfoley@users.noreply.github.com>
2022-03-18 14:38:02 +0000

    Update docs_transportzones.py

--- lib/docs_transportzones.py
+++ lib/docs_transportzones.py
@@ -35,15 +35,49 @@ from lib.system import style, GetAPI, ConnectNSX, os, GetOutputFormat
 
 
 def SheetTZ(auth_list,WORKBOOK,TN_WS, NSX_Config ={} ):
-    NSX_Config['TZ'] = []
-    # Connect NSX
     SessionNSX = ConnectNSX(auth_list)
-    transport_zone_url = '/api/v1/transport-zones'
-    transport_zone_json = GetAPI(SessionNSX[0],transport_zone_url, auth_list)
-    XLS_Lines = []
-    TN_HEADER_ROW = ('Name', 'ID', 'Ressource Type', 'Host Switch ID', 'Host Switch Mode', 'Host Switch Name', 'Host Switch is Default', 'is Nested NSX', 'Transport Type')
-    if isinstance(transport_zone_json, dict) and 'results' in transport_zone_json and transport_zone_json['result_count'] > 0: 
-        for TZ in transport_zone_json['results']:
+    node_url = '/api/v1/node'
+    node_result = GetAPI(SessionNSX[0],node_url, auth_list)
+    node_version = node_result["product_version"]
+    node_v = node_version[0:3]
+    if '3.2' in  node_v:
+       print(node_v)
+       NSX_Config['TZ'] = []
+       # Connect NSX
+       SessionNSX = ConnectNSX(auth_list)
+       transport_zone_url = '/api/v1/transport-zones'
+       transport_zone_json = GetAPI(SessionNSX[0],transport_zone_url, auth_list)
+       XLS_Lines = []
+       TN_HEADER_ROW = ('Name', 'ID', 'Ressource Type', 'Host Switch ID', 'Host Switch Mode', 'Host Switch Name', 'Host Switch is Default', 'is Nested NSX', 'Transport Type')
+       if isinstance(transport_zone_json, dict) and 'results' in transport_zone_json and transport_zone_json['result_count'] > 0:
+           for TZ in transport_zone_json['results']:
+               Dict_TZ = {}
+               Dict_TZ['name'] = TZ['display_name']
+               Dict_TZ['id'] = TZ['id']
+               Dict_TZ['resource_type'] = TZ['resource_type']
+               # Dict_TZ['host_swithc_id'] = TZ['host_switch_id']
+               # Dict_TZ['host_switch_mode'] = TZ['host_switch_mode']
+               # Dict_TZ['host_switch_name'] = TZ['host_switch_name']
+               Dict_TZ['is_default'] = TZ['is_default']
+               Dict_TZ['nested'] = TZ['nested_nsx']
+               Dict_TZ['type'] = TZ['transport_type']
+               NSX_Config['TZ'].append(Dict_TZ)
+               # Create line
+               XLS_Lines.append([TZ['display_name'], TZ['id'], TZ['resource_type'], 'Not present in 3.2 API Call', 'Not present in 3.2 API Call', 'Not present in 3.2 API Call', TZ['is_default'], TZ['nested_nsx'], TZ['transport_type']])
+       else:
+           XLS_Lines.append(['no Transport Zones', '', '', '', '', '', '', '', ''])
+
+    else:
+       NSX_Config['TZ'] = []
+       # Connect NSX
+       SessionNSX = ConnectNSX(auth_list)
+       transport_zone_url = '/api/v1/transport-zones'
+       transport_zone_json = GetAPI(SessionNSX[0],transport_zone_url, auth_list)
+       # if node_version 
+       XLS_Lines = []
+       TN_HEADER_ROW = ('Name', 'ID', 'Ressource Type', 'Host Switch ID', 'Host Switch Mode', 'Host Switch Name', 'Host Switch is Default', 'is Nested NSX', 'Transport Type')
+       if isinstance(transport_zone_json, dict) and 'results' in transport_zone_json and transport_zone_json['result_count'] > 0: 
+          for TZ in transport_zone_json['results']:
             Dict_TZ = {}
             Dict_TZ['name'] = TZ['display_name']
             Dict_TZ['id'] = TZ['id']
@@ -57,8 +91,8 @@ def SheetTZ(auth_list,WORKBOOK,TN_WS, NSX_Config ={} ):
             NSX_Config['TZ'].append(Dict_TZ)
             # Create line
             XLS_Lines.append([TZ['display_name'], TZ['id'], TZ['resource_type'], TZ['host_switch_id'], TZ['host_switch_mode'], TZ['host_switch_name'], TZ['is_default'], TZ['nested_nsx'], TZ['transport_type']])
-    else:
-        XLS_Lines.append(['no Transport Zones', '', '', '', '', '', '', '', ''])
+       else:
+          XLS_Lines.append(['no Transport Zones', '', '', '', '', '', '', '', ''])
 
     if GetOutputFormat() == 'CSV':
         CSV = WORKBOOK
