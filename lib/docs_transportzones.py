890d320647985029bde548cc941c4321fcdc2b0d
Yann Simonet <ysimonet@gmail.com>
2022-04-04 11:03:46 +0200

    Bug fix on replication-mode and hostswitch
    Bug fix on replication-mode and hostswitch

--- lib/docs_transportzones.py
+++ lib/docs_transportzones.py
@@ -28,37 +28,54 @@
 # *--------------------------------------------------------------------------------------* #                                                                                                #
 #                                                                                                                                                                                           #
 #############################################################################################################################################################################################
-import pathlib, lib.menu
+import pathlib, lib.menu, pprint
 from lib.excel import FillSheet, Workbook, FillSheetCSV, FillSheetJSON, FillSheetYAML
 from lib.system import style, GetAPI, ConnectNSX, os, GetOutputFormat
 
 
 
 def SheetTZ(auth_list,WORKBOOK,TN_WS, NSX_Config ={} ):
+    SessionNSX = ConnectNSX(auth_list)
     NSX_Config['TZ'] = []
+    node_url = '/api/v1/node'
+    node_result = GetAPI(SessionNSX[0],node_url, auth_list)
+    node_version = node_result["product_version"]
+    node_v = node_version[0:3]
     # Connect NSX
     SessionNSX = ConnectNSX(auth_list)
     transport_zone_url = '/api/v1/transport-zones'
     transport_zone_json = GetAPI(SessionNSX[0],transport_zone_url, auth_list)
+    # Error message for 3.2
+    error_msg = 'Not present in ' + node_v + ' API Call'
+
     XLS_Lines = []
     TN_HEADER_ROW = ('Name', 'ID', 'Ressource Type', 'Host Switch ID', 'Host Switch Mode', 'Host Switch Name', 'Host Switch is Default', 'is Nested NSX', 'Transport Type')
     if isinstance(transport_zone_json, dict) and 'results' in transport_zone_json and transport_zone_json['result_count'] > 0: 
-        for TZ in transport_zone_json['results']:
-            Dict_TZ = {}
-            Dict_TZ['name'] = TZ['display_name']
-            Dict_TZ['id'] = TZ['id']
-            Dict_TZ['resource_type'] = TZ['resource_type']
-            Dict_TZ['host_swithc_id'] = TZ['host_switch_id']
+       for TZ in transport_zone_json['results']:
+        Dict_TZ = {}
+        Dict_TZ['name'] = TZ['display_name']
+        Dict_TZ['id'] = TZ['id']
+        Dict_TZ['resource_type'] = TZ['resource_type']
+        if 'host_switch_id' in Dict_TZ: 
+            Dict_TZ['host_switch_id'] = TZ['host_switch_id']
+        else:
+            Dict_TZ['host_switch_id'] = error_msg
+        if 'host_switch_mode' in Dict_TZ: 
             Dict_TZ['host_switch_mode'] = TZ['host_switch_mode']
+        else:
+            Dict_TZ['host_switch_mode'] = error_msg
+        if 'host_switch_name' in Dict_TZ: 
             Dict_TZ['host_switch_name'] = TZ['host_switch_name']
-            Dict_TZ['is_default'] = TZ['is_default']
-            Dict_TZ['nested'] = TZ['nested_nsx']
-            Dict_TZ['type'] = TZ['transport_type']
-            NSX_Config['TZ'].append(Dict_TZ)
-            # Create line
-            XLS_Lines.append([TZ['display_name'], TZ['id'], TZ['resource_type'], TZ['host_switch_id'], TZ['host_switch_mode'], TZ['host_switch_name'], TZ['is_default'], TZ['nested_nsx'], TZ['transport_type']])
+        else:
+            Dict_TZ['host_switch_name'] = error_msg
+        Dict_TZ['is_default'] = TZ['is_default']
+        Dict_TZ['nested'] = TZ['nested_nsx']
+        Dict_TZ['type'] = TZ['transport_type']
+        NSX_Config['TZ'].append(Dict_TZ)
+        # Create line
+        XLS_Lines.append([Dict_TZ['name'], Dict_TZ['id'], Dict_TZ['resource_type'], Dict_TZ['host_switch_id'], Dict_TZ['host_switch_mode'], Dict_TZ['host_switch_name'], Dict_TZ['is_default'], Dict_TZ['nested'], Dict_TZ['type']])
     else:
-        XLS_Lines.append(['no Transport Zones', '', '', '', '', '', '', '', ''])
+       XLS_Lines.append(['no Transport Zones', '', '', '', '', '', '', '', ''])
 
     if GetOutputFormat() == 'CSV':
         CSV = WORKBOOK
